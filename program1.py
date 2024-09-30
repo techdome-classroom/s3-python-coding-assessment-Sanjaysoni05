933d2a7d74df135d229c62bfc1333aa5a2a01875
Sanjaysoni05 <164004598+Sanjaysoni05@users.noreply.github.com>
2024-09-30 15:58:21 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,5 +1,23 @@
 class Solution(object):
-    def isValid(self, s):
+    def isValid(self, s)->bool:
+        bracaket_map={'):'(,'}:'{',']':'['}
+        stack=[]
+        for char in s:
+        if char in bracket_map:
+        top_element=stack.pop() if stack else'#'
+        if brackert_map[char]
+        !=top_element:
+        return false
+        else:
+          stack.apppend(char)
+        return not stack
+        print(isValid("()"))
+        print(isValid("()[]{}"))
+        print(isValid("(]"))
+        print(isValid("([)]"))
+        print(isValid("{[]}"))
+        
+        
         """
         :type s: str
         :rtype: bool
