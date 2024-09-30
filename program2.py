f2f0057244f5cda45971f2b6e64feeb0cce0ae5b
Sanjaysoni05 <164004598+Sanjaysoni05@users.noreply.github.com>
2024-09-30 16:08:09 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,19 @@
 class Solution(object):
-    def romanToInt(self, s):
+    def romanToInt(self, s -)>int:
+    roman_map={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    total=0
+    for i in range (lane(s)):
+        if i+1<lane(s) and roman_map[s[i]]<roman_map[s[i]]
+        else:
+            total +=roman_map[s[i]]
+            return toatal
+            print(romanTOInt("III"))
+            print(romanTOInt("LVIII"))
+            print(romanTOInt("MCMXCIV"))
+        
+        
         """
-        :type s: str
+         :type s: str
         :rtype: int
         """
         pass
